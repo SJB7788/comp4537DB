ae92554ce9e6927997def76565f555b2eaad33fb
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-10-13 21:02:13 -0700

    bugfix client routing issues

--- app.js
+++ app.js
@@ -2,16 +2,32 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   if (req.url == "/") {
-    fs.readFile("./public/index.html", (err, html) => {
-      if (err) {
-        throw err;
-      }
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
+    const indexHtml = fs.readFileSync("./public/index.html");
+    res.writeHead(200, {
+      "Content-Type": "text/html",
     });
+    res.end(indexHtml);
+    return;
+  }
+
+  if (req.url.startsWith("/css")) {
+    switch (req.url) {
+      case "/css/style.css":
+        const css = fs.readFileSync("./public/css/style.css");
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(css)
+    }
+  }
+
+  if (req.url.startsWith("/js")) {
+    switch (req.url) {
+      case "/js/index.js":
+        const js = fs.readFileSync("./public/js/index.js");
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        res.end(js);
+    }
   }
 });
 
